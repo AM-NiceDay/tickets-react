875d03fe9ca813aa3de0880ab8f5b93159d49bad
Aliaksei Krasikau <a.krasikau@gmail.com>
2015-12-02 20:18:48 +0300

    Add IndexWithTicket component with test

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import IndexWithTicket from './components/IndexWithTicket';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const ticket = {
+  id: 123,
+  dateCreated: new Date()
+};
+
+ReactDOM.render(<IndexWithTicket ticket={ticket} />, document.getElementById('app'));
