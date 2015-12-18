a3bf5f11e20dd3dc73c93e638951c14111d7d4b7
Aliaksei Krasikau <a.krasikau@gmail.com>
2015-12-19 00:17:53 +0300

    Create Index component

---
+++ src/containers/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default React.createClass({
+  render() {
+    return <div>
+      <button>Sign In</button>
+      <button>Sign Up</button>
+    </div>;
+  }
+});
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import Index from './containers/Index';
 
 ReactDOM.render(
-  <App />,
+  <Index />,
   document.getElementById('app'));
