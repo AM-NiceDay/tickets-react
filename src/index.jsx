84dfcb8505eefad6af5edde409aaa59ba93b4dcb
Aliaksei Krasikau <a.krasikau@gmail.com>
2015-12-19 00:40:38 +0300

    Setup browser history

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute } from 'react-router';
+import history from './history';
 import App from './containers/App';
 import Index from './containers/Index';
 
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <Route path="/" component={App}>
       <IndexRoute component={Index} />
     </Route>
