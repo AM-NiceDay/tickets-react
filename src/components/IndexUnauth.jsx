f8fd3cbc882edef7edd128fd33bf35e94e0a1358
Aliaksei Krasikau <a.krasikau@gmail.com>
2015-12-19 11:47:30 +0300

    Substitute Index buttons with react-router links

--- src/components/IndexUnauth.jsx
+++ src/components/IndexUnauth.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router';
 import history from '../history';
 
 export default React.createClass({
   render() {
     return <div>
-      <button onClick={() => history.pushState(null, '/signin')}>Sign In</button>
-      <button>Sign Up</button>
+      <Link to="/signin">Sign In</Link>
+      {' '}
+      <Link to="/signup">Sign Up</Link>
     </div>;
   }
 });
