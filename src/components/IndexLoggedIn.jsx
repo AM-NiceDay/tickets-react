133c2a1e8bbf9db147235d17c08854dcdab5ab7f
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-02-15 20:52:17 +0300

    Show button if ticket already exist

--- src/components/IndexLoggedIn.jsx
+++ src/components/IndexLoggedIn.jsx
@@ -10,9 +10,9 @@ export default class IndexLoggedIn extends React.Component {
     return <div>
       <p>Hello, { this.props.phoneNumber }</p>
       <button onClick={ this.props.logout }>Logout</button>
+      <button onClick={ this.props.transmitToBuy }>Buy ticket</button>
       { this.isTicketExists() ?
-        <p>Ticket: { this.props.ticket.get('busId') } - { this.props.ticket.get('created') }</p> :
-        <button onClick={ this.props.transmitToBuy }>Buy ticket</button>
+        <p>Ticket: { this.props.ticket.get('busId') } - { this.props.ticket.get('created') }</p> : null
       }
     </div>;
   }
