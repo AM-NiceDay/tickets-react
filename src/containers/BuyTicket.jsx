0728b7462dc09b4f0b65f40cd89d070b610211db
Aliaksei Krasikau <a.krasikau@gmail.com>
2016-02-20 11:47:08 +0300

    Refactor BuyTicket component

--- src/containers/BuyTicket.jsx
+++ src/containers/BuyTicket.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { pushPath } from 'redux-simple-router';
 import { checkBus, uncheckBus } from '../actions/bus';
 import { buyTicket } from '../actions/ticket';
 import CheckMessage from '../components/CheckMessage';
+import { Link } from 'react-router';
 
-const BuyTicket = React.createClass({
+class BuyTicket extends Component {
 
-  handleCheckBus() {
+  constructor(props) {
+    super(props);
+
+    this.checkBusHandler = this.checkBusHandler.bind(this);
+    this.buyTicketHandler = this.buyTicketHandler.bind(this);
+  }
+
+  checkBusHandler() {
     const { dispatch } = this.props;
     const busCode = this.refs.busCode.value;
 
@@ -16,9 +24,11 @@ const BuyTicket = React.createClass({
     } else {
       dispatch(uncheckBus());
     }
-  },
+  }
 
-  handleBuyTicket() {
+  buyTicketHandler(e) {
+    e.preventDefault();
+
     const { dispatch, user } = this.props;
     const { checked, exist } = this.props.bus.toJS();
     const busCode = this.refs.busCode.value;
@@ -27,26 +37,31 @@ const BuyTicket = React.createClass({
       dispatch(buyTicket(user.get('_id'), busCode)).payload.promise
         .then(result => {
           if (!result.error) {
-            dispatch(pushPath('/'));
+            dispatch(pushPath('/ticket'));
           }
         });
     }
-  },
+  }
 
   render() {
-    const { checked, exist, route, routeName, loading } = this.props.bus.toJS();
-    const checkProps = { checked, exist, route, routeName, loading };
+    const { checked, exist } = this.props.bus.toJS();
 
-    return <div>
-      <input type="text" ref="busCode" placeholder="Bus Id" onKeyUp={ this.handleCheckBus } />
-      <CheckMessage { ...checkProps } />
-      { this.props.ticket.get('loading') ?
-        <span>Loading</span> :
-        <button disabled={ !checked || !exist } onClick={ this.handleBuyTicket }>Buy</button>
-      }
-    </div>;
+    return (
+      <div>
+        <div>
+          <Link to="/ticket">{'<-'}</Link>
+          <span>Штрих код</span>
+        </div>
+        <form onSubmit={this.buyTicketHandler} >
+          <p>Введите четырехзначный код, размещенный в автотранспорте</p>
+          <p><input type="text" ref="busCode" onKeyUp={this.checkBusHandler} /></p>
+          <p>С Вашего счета будет списано 4 650 руб</p>
+          <button type="submit" disabled={!checked || !exist}>Подтвердить</button>
+        </form>
+      </div>
+    );
   }
-});
+}
 
 export default connect(state => ({
   user: state.user,
